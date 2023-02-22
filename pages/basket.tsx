a90a2e795151b586155d8e93edbb754f8bc41352
Bartłomiej Rusoń <bartlomiej.ruson@gmail.com>
2023-02-22 09:44:09 +0100

    checkout functionality

--- pages/basket.tsx
+++ pages/basket.tsx
@@ -1,47 +1,76 @@
+import { useSession,signIn } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Nav from "../components/Nav";
-import { removeFromBasket, selectItems } from "../slices/basketSlice";
+import {
+  removeFromBasket,
+  selectItems,
+  selectTotalPrice,
+} from "../slices/basketSlice";
 function Basket() {
+  const { data: session } = useSession()
   const items = useSelector(selectItems);
+  const totalPrice = useSelector(selectTotalPrice);
   const dispatch = useDispatch();
   return (
     <div>
       <Nav></Nav>
       <div className="max-w-screen-2xl mx-auto p-3 text-center">
         <h1 className="text-3xl mt-2 mb-5">
-          {items.length !== 0 ? "Your Basket" : "Your basket is empty"}
+          {items.length !== 0 ? "Your Basket. " : "Your basket is empty"}{" "}
+          {items.length !== 0 ? `Total price is ${totalPrice} $` : ""}
         </h1>
-        {items.map(({ id, title, price, image, description }) => (
-          <div
-            key={id}
-            className="border flex justify-between flex-row m-3 rounded-sm bg-white"
-          >
-            <Image
-              src={image}
-              width={200}
-              height={200}
-              className="object-contain p-3 mr-auto"
-              alt={title}
-            ></Image>
-            <div className="mr-auto">
-              <h2 className="p-2 font-semibold ">{title}</h2>
-              <h2 className="font-semibold ">{price}$</h2>
-              <h2 className="pt-2 line-clamp-3">{description}</h2>
-              <button
-                onClick={() => {
-
-                  dispatch(removeFromBasket(id));
-                }}
-                className="p-2 my-3 mx-3 font-semibold opacity-90 hover:opacity-100 transition-all text-white bg-darkRed rounded-md"
-              >
-                REMOVE FROM BASKET
-              </button>
+        <main className="grid grid-cols-6">
+          <div className="col-span-5 mr-5">
+          {items.map(({ id, title, price, image, description }) => (
+            <div
+              key={id}
+              className="border flex justify-between flex-row m-3 rounded-sm bg-white"
+            >
+              <Image
+                src={image}
+                width={200}
+                height={200}
+                className="object-contain p-3 mr-auto"
+                alt={title}
+              ></Image>
+              <div className="mr-auto">
+                <h2 className="p-2 font-semibold ">{title}</h2>
+                <h2 className="font-semibold ">{price}$</h2>
+                <h2 className="pt-2 line-clamp-3">{description}</h2>
+                <button
+                  onClick={() => {
+                    dispatch(removeFromBasket({ id, price }));
+                  }}
+                  className="p-2 my-3 mx-3 font-semibold opacity-90 hover:opacity-100 transition-all text-white bg-darkRed rounded-md"
+                >
+                  REMOVE FROM BASKET
+                </button>
+              </div>
             </div>
+          ))}
           </div>
-        ))}
+          {items.length>0?          <div className="flex flex-col space-y-4 text-lg">
+            <h1>Order Summary</h1>
+            <h1>{`(${items.length}) `}{ items.length>=2 ?'items: ':"item: "} {totalPrice} $</h1>
+            <h1 className="border-b-2 pb-1">Delivery: Free</h1>
+            <h1 className="font-bold text-xl">Total: {totalPrice} $</h1>
+            {session?            <button
+            className="p-2 my-3 mx-3 font-semibold opacity-90 hover:opacity-100 transition-all text-white bg-darkRed rounded-md"
+          >
+            CHECK OUT
+          </button>:
+                      <button onClick={()=>{signIn()}}
+                      className="p-2 my-3 mx-3 font-semibold opacity-90 hover:opacity-100 transition-all text-white bg-darkRed rounded-md"
+                    >
+                     SIGN IN TO CHECK OUT
+                    </button>}
+
+          </div>:null}
+
+        </main>
       </div>
     </div>
   );
--- slices/basketSlice.ts
+++ slices/basketSlice.ts
@@ -3,7 +3,8 @@ import { Product } from '../interfaces'
 type ProductArray = Array<Product>
 
 const initialState  = {
-  items:[]
+  items:[],
+  totalPrice:0
 }
 
 const basketSlice = createSlice({
@@ -11,14 +12,16 @@ const basketSlice = createSlice({
   initialState,
   reducers: {
     addToBasket: (state,action) => {
-state.items=[...state.items,action.payload]
+state.items=[...state.items,action.payload];
+state.totalPrice=state.totalPrice + action.payload.price;
     },
     removeFromBasket:(state,action)=>{
 
-        const itemId = state.items.findIndex(item=>item.id===action.payload)
+        const itemId = state.items.findIndex(item=>item.id===action.payload.id)
         let newState = [...state.items]
         if (itemId > -1) { 
            newState.splice(itemId, 1); 
+          state.totalPrice= state.totalPrice - action.payload.price
         } else {
           console.log("cant find a product to remove")
         }
@@ -29,4 +32,5 @@ state.items=[...state.items,action.payload]
 
 export const { addToBasket,removeFromBasket } = basketSlice.actions
 export const selectItems = (state) => state.basket.items;
+export const selectTotalPrice = (state) => state.basket.totalPrice;
 export default basketSlice.reducer
